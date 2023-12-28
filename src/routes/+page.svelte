b201b1d38019687e46ed54a90f403aa2f3196ac7
Jose Von Chong <javch1995@gmail.com>
2023-12-28 11:40:54 -0500

    updates on texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Inicio</a>
+					</li>
+                    <li class="text-grey-75 font-sans">›</li>
+                        <li class="text-grey-75 font-sans hover:underline active:underline">
+                            <a href="destinationsguide.copaair.com/en/deals">Promociones</a>
+                        </li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Vuelos para vacaciones</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Descubre tus destinos soñados con nuestras tarifas en vuelos para tus vacaciones </h2>
+        </div>
+	</div>
+</div>
+
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Home</a>
+					</li>
+                    <li class="text-grey-75 font-sans">›</li>
+                        <li class="text-grey-75 font-sans hover:underline active:underline">
+                            <a href="destinationsguide.copaair.com/en/deals">Deals</a>
+                        </li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Flights for vacations</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Discover your dream destinations with our flight fares for your vacation.</h2>
+        </div>
+	</div>
+</div>
+
+
+<div class="container justify-items-center m-auto">
+    <div class="mt-16">
+		<h2 class="text-primary font-heading-bold font-heading text-h2 sm:text-h2-sm md:text-h2-md lg:text-h2-lg !mb-16">
+			Tus vacaciones soñadas comienzan aquí
+		  </h2>
+        <p class="text-body-1 ">Prepárate para unas vacaciones inolvidables. Encuentra la ruta perfecta para tu escape soñado usando nuestro buscador de viajes personalizado. Descubre destinos increíbles y vive la escapada que siempre has imaginado</p>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// Used for testing styles
 
+/** @type {import('tailwindcss').Config} */
+const cmpreset = require('cmds-tailwind-styles');
+
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/cmds-component-*/*.{html,js,svelte,ts}'
+	],
+	presets: [cmpreset]
+};
